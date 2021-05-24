5f9850bae782b72dbbf538959a0d531cd6bff3e6
Luis Garcia <jrluis21@gmail.com>
2021-05-23 23:01:39 -0500

    created users, recipes, steps, ingredients tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,56 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
+    .createTable('users', (table) => {
+      table.increments('user_id')
+      table.string('user_username', 200).notNullable()
+      table.string('user_password', 200).notNullable()
+      table.string('user_email', 320).notNullable()
+      table.timestamps(false, true)
     })
+
+    .createTable('recipes', (table) => {
+      table.increments('recipe_id');
+      table.string('recipe_name', 128).notNullable().unique();//should this be unique
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('user_id')
+        .inTable('users')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT');
+    })
+
+    .createTable('steps', (table) => {
+      table.increments('step_id');
+      table.text('instruction_description').notNullable();
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT');
+    })
+
+    .createTable('ingredients', (table) => {
+      table.increments('ingredient_id');
+      table.string('ingredient_name', 128).notNullable().unique();
+      table
+        .integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT');
+    });
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('recipes')
+  await knex.schema.dropTableIfExists('steps')
+  await knex.schema.dropTableIfExists('ingredients')
 }
